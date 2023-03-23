11cc54ebd27266ba5b9c552cfd00ce7761a8fe3d
Marsel Basyrov <m.basirov@steiza.com>
2023-03-23 17:35:44 +0300

    Анимация и стрельба
    Добавил анимацию взгляда и наведения орузжия верх и вниз, прописал первичный алгоритм вращения спавнера пуль

--- TarakansTesting/Assets/Script/GunAndAmmo/Shooting.cs
+++ TarakansTesting/Assets/Script/GunAndAmmo/Shooting.cs
@@ -2,13 +2,30 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    [SerializeField] private CameraCharacteristics _camCharacteristics;
+
     [SerializeField] private ShootingCharacteristics _shooting; //Ссылка на значения характеристик из другого скрипта
     
     [SerializeField] private Rigidbody _ammo;
 
     [SerializeField] private Transform _spawnPoint;
 
-    void Update()
+    [SerializeField] public float yDeg = 0.0f;
+
+    [SerializeField] private float xDeg = 0.0f;
+
+
+    private Quaternion _look; //Переменная для записи поворота персонажа
+    
+    private void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        yDeg = angles.y;
+        xDeg = angles.x;
+
+    }
+
+    private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
@@ -18,5 +35,9 @@ public class Shooting : MonoBehaviour
             // Add shooting sound effect here
             Debug.Log("Игрок выстрелил");
         }
+        xDeg += Input.GetAxis("Mouse X") * _camCharacteristics.xSpeed * 0.02f;
+        yDeg = Mathf.Clamp(yDeg, _camCharacteristics.MinVerticalAngle, _camCharacteristics.MaxVerticalAngle); // ограничиваем угол возвышения камеры        
+        yDeg -= Input.GetAxis("Mouse Y") * _camCharacteristics.ySpeed * 0.02f;   
+        _spawnPoint.transform.rotation = Quaternion.Euler(yDeg, xDeg, 0);
     }
 }
