e89268e918b9f10b1d8bae48419ae316a5aa8c58
Marsel Basyrov <m.basirov@steiza.com>
2023-03-28 22:31:06 +0300

    Сделал камеру и стрельбу
    Сделал камеру сбоку, стала более динамичной и стрельбу в точку по центру экрана (относительно)

--- TarakansTesting/Assets/Script/Player/WoWcamera.cs
+++ TarakansTesting/Assets/Script/Player/WoWcamera.cs
@@ -12,84 +12,95 @@ public class WoWcamera : MonoBehaviour
 
     [SerializeField] private CameraCharacteristics _camCharacteristics;
 
-    private float currentDistance;
-    private float desiredDistance;
-    private float correctedDistance;
+    [Header("Смещение в бок")]
+
+    [Space(10)]
+
+	[SerializeField] private float offsetPosition; // смешение камеры вправо или влево, 0 = центр
+
+    [Header("Основные настройки")]
+
+    [Space(10)]
+
+    [SerializeField] private float distance = 1f;
+
+    [SerializeField] private float height = 2.3f;
+
+    [SerializeField] private enum Smooth {Disabled = 0, Enabled = 1};
+	[SerializeField] private Smooth smooth = Smooth.Enabled;
+	[SerializeField] private float speed = 8; // скорость сглаживания
+
+    // private float desiredDistance;
+    // private float correctedDistance;
 
     private Animator _animator;
 
-    private bool isLookingUp = false;
-    private bool isLookingDown = false;
-
     private float xDeg = 0.0f;
-    public float yDeg = 0.0f;
+    private float yDeg = 0.0f;
        
     private void Start ()
     {
         _animator = target.GetComponent<Animator>(); //Инициализируем аниматор
         Cursor.lockState = CursorLockMode.Locked; //Блокируем курсор по центру
-        Vector3 angles = transform.eulerAngles; 
-        xDeg = angles.x;
-        yDeg = angles.y;
-        
-        currentDistance = _camCharacteristics.distance;
-        desiredDistance = _camCharacteristics.distance;
-        correctedDistance = _camCharacteristics.distance;
+
+        // desiredDistance = _camCharacteristics.distance;
+        // correctedDistance = _camCharacteristics.distance;
         
         if (GetComponent<Rigidbody>())
             GetComponent<Rigidbody>().freezeRotation = true; // Блокируем поворот у компонента Rigidbody
     }
 
-    private void Update ()
+    private void LateUpdate ()
     {
-        Vector3 vTargetOffset;
-        
         // Проверяем, если наш персонаж не обнаружен на сцене, камера ничего делать не будет
         if (!target)
                 return;
 
+        
+        xDeg += Input.GetAxis("Mouse X") * _camCharacteristics.xSpeed * 0.02f;
+        yDeg -= Input.GetAxis("Mouse Y") * _camCharacteristics.ySpeed * 0.02f;
         yDeg = Mathf.Clamp(yDeg, _camCharacteristics.MinVerticalAngle, _camCharacteristics.MaxVerticalAngle); // ограничиваем угол возвышения камеры        
         
-        //При движении мыши будет изменяться переменная координаты, которая будет умножаться на скорость камеры
-        xDeg += Input.GetAxis("Mouse X") * _camCharacteristics.xSpeed * 0.02f; 
-        yDeg -= Input.GetAxis("Mouse Y") * _camCharacteristics.ySpeed * 0.02f;
 
         // Передаем в аниматор изменение координаты по У
         _animator.SetFloat("WeaponUp", yDeg);
            
         // Через кватернион подключаем поворот камеры
-        Quaternion rotation = Quaternion.Euler (yDeg, xDeg, 0);
+        Quaternion rotation = Quaternion.Euler(yDeg, xDeg, 0);
         
         // Логика приближения камеры при помощи колесика мыши
-        desiredDistance -= Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime * _camCharacteristics.zoomRate * Mathf.Abs (desiredDistance);
-        desiredDistance = Mathf.Clamp (desiredDistance, _camCharacteristics.minDistance, _camCharacteristics.maxDistance);
-        correctedDistance = desiredDistance;
-        
-        // Расчёт нужного нам расстояния
-        vTargetOffset = new Vector3 (0, -_camCharacteristics.targetHeight, 0);
-        Vector3 position = target.position - (rotation * Vector3.forward * desiredDistance + vTargetOffset);
-        
-        // Используя лучи проверяем соприкосновения с физическими объектами
-        RaycastHit collisionHit;
-        Vector3 trueTargetPosition = new Vector3 (target.position.x, target.position.y + _camCharacteristics.targetHeight, target.position.z);
-        
-        // Если камера сталкивается с физическим объектом, дистанция от камеры до игрока корректируется
-        bool isCorrected = false;
-        if (Physics.Linecast (trueTargetPosition, position, out collisionHit, _camCharacteristics.collisionLayers.value))
-        {
-            correctedDistance = Vector3.Distance (trueTargetPosition, collisionHit.point) - _camCharacteristics.offsetFromWall;
-            isCorrected = true;
-        }
-           
-        currentDistance = !isCorrected || correctedDistance > currentDistance ? Mathf.Lerp (currentDistance, correctedDistance, Time.deltaTime * _camCharacteristics.zoomDampening) : correctedDistance;
-        
-        currentDistance = Mathf.Clamp (currentDistance, _camCharacteristics.minDistance, _camCharacteristics.maxDistance);
-        
-        position = target.position - (rotation * Vector3.forward * currentDistance + vTargetOffset);
-        
+        // desiredDistance -= Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime * _camCharacteristics.zoomRate * Mathf.Abs (desiredDistance);
+        // desiredDistance = Mathf.Clamp (desiredDistance, _camCharacteristics.minDistance, _camCharacteristics.maxDistance);
+        // correctedDistance = desiredDistance;
+
+        Vector3 position = target.position - (rotation * Vector3.forward * distance);
+        position = position + (rotation * Vector3.right * offsetPosition); // корректировка горизонтали
+        position = new Vector3(position.x, target.position.y + height, position.z); // корректировка высоты
+        position = PositionCorrection(target.position, position);
+
         transform.rotation = rotation;
-        transform.position = position;
+        if(smooth == Smooth.Disabled) 
+        {
+            transform.position = position;
+        }
+		else 
+        {
+            transform.position = Vector3.Lerp(transform.position, position, speed * Time.deltaTime);
+        }
+        
     }
+    Vector3 PositionCorrection(Vector3 Qtarget, Vector3 position)
+	{
+		RaycastHit hit;
+		Debug.DrawLine(Qtarget, position, Color.blue);
+		if(Physics.Linecast(Qtarget, position, out hit)) 
+		{
+			float tempDistance = Vector3.Distance(Qtarget, hit.point);
+			Vector3 pos = Qtarget - (transform.rotation * Vector3.forward * tempDistance);
+			position = new Vector3(pos.x, position.y, pos.z); // сдвиг позиции в точку контакта
+		}
+		return position;
+	}
 
 }
  
